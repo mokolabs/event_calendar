ace77aa8906e232ad84cdb916058bf50a674e199
Patrick Crowley <patrick@mokolabs.com>
2009-12-03 13:56:42 -0800

    fingers crossed

--- lib/event_calendar.rb
+++ lib/event_calendar.rb
@@ -45,7 +45,7 @@ module EventCalendar
     
     # Get the events overlapping the given start and end dates
     def events_for_date_range(start_d, end_d)
-      calendar = params[:id].to_i || 1084
+      logger.info request.inspect
       self.find(
         :all,
         :conditions => [ 'calendar_id = ? AND (? <= end_at) AND (start_at < ?)', calendar, start_d.to_time.utc, end_d.to_time.utc ],
