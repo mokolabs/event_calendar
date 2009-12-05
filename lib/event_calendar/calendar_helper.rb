49c6804b50426c893de5038db7ace29220b26851
Patrick Crowley <patrick@mokolabs.com>
2009-12-05 11:50:47 -0800

    removed the line break adding code

--- lib/event_calendar/calendar_helper.rb
+++ lib/event_calendar/calendar_helper.rb
@@ -235,9 +235,6 @@ module EventCalendar
       cal << %(</div>)
       cal << %(</div>)
       
-      # Force new tags to the next line
-      cal.gsub(/></, ">\n<")
-      
     end
     
     # override this in your own helper for greater control
